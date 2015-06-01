1f0a0644e465ddae22ffcde5c1d8654e5b685532
lilybader <vagrant@precise32.(none)>
2015-06-01 21:13:41 +0000

    Used jQuery to populate album view.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -90,9 +90,82 @@
   globals.require.list = list;
   globals.require.brunch = true;
 })();
-require.register("scripts/app", function(exports, require, module) {
+require.register("scripts/album", function(exports, require, module) {
+var albumPicasso = {
+	name: 'The Colors',
+	artist: 'Pablo Picasso',
+	label: 'Cubism',
+	year: '1881',
+	albumArtUrl: '/images/album-placeholder.png',
+	songs: [
+	{ name: 'Blue', length: '4:26'},
+	{ name: 'Green', length: '3:14'},
+	{ name: 'Red', length: '5:01' },
+    { name: 'Pink', length: '3:21'},
+    { name: 'Magenta', length: '2:15'}
+	]
+};
+
+var albumMarconi = {
+   name: 'The Telephone',
+   artist: 'Guglielmo Marconi',
+   label: 'EM',
+   year: '1909',
+   albumArtUrl: '/images/album-placeholder.png',
+   songs: [
+       { name: 'Hello, Operator?', length: '1:01' },
+       { name: 'Ring, ring, ring', length: '5:01' },
+       { name: 'Fits in your pocket', length: '3:21'},
+       { name: 'Can you hear me now?', length: '3:14' },
+       { name: 'Wrong phone number', length: '2:15'}
+     ]
+ };
+
+var createSongRow = function(songNumber, songName, songLength) {
+	var template = 
+		'<tr>'
+		+ ' <td class="col-md-1">' + songNumber + '</td>'
+		+ ' <td class="col-md-9">' + songName + '</td>'
+		+ ' <td class="col-md-2">' + songLength + '</td>'
+		+ ' </tr>'
+		;
+	return $(template);
+	};
+
+var changeAlbumView = function(album){
+	var $albumTitle = $('.album-title');
+	$albumTitle.text(album.name);
+
+	var $albumArtist = $('.album-artist');
+	$albumArtist.text(album.artist);
+
+	var $albumMeta = $('.album-meta-info');
+	$albumMeta.text(album.year + ' on ' + album.label);
+
+	var $albumImage = $('.album-image');
+	$albumImage.attr('src', album.albumArtUrl);
+
+	var $songList = $('.album-song-listing');
+	$songList.empty();
+	var songs = album.songs;
+	for (var i = 0; i < song.length; i++) {
+		var songData = songs[i];
+		var $newRow = createSongRow(i + 1, songData.name, songData.length);
+		$songList.append($newRow);
+	}
+};
+
+if (document.URL.match(/\/album.html/)) {
+	$(document).ready(function() {
+		changeAlbumView(albumPicasso);
+	});
+}
+});
+
+;require.register("scripts/app", function(exports, require, module) {
 require("./landing");
 require('./collection');
+require('./album');
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -159,9 +232,9 @@ function updateCollectionView() {
 };
 
 if (document.URL.match(/\/collection.html/)) {
-  // Wait until the HTML is fully processed.
+  
   $(document).ready(function() {
-    // code goes here
+    
     updateCollectionView();
   });
 }
